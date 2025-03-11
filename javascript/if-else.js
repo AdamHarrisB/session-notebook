19e01df87da6af49446777e33dda8cd22d5dbbe5
Adam Harris <adamtjharris@gmail.com>
2025-03-11 09:32:53 +0100

    adding notes

---
+++ javascript/if-else.js
@@ -0,0 +1,17 @@
+//let cookies = 2;
+//let runKm = 4;
+//const wentToGym = true;
+
+//if(runKm = 4){
+    cookies = cookies + 2;
+    console.log("first condition");
+    console.log(cookies);
+//} else if(runKm === 4 && wentToGym){
+    cookies += 4;
+    console.log("second condition");
+//} else {
+    cookies--;
+    console.log("third condition");
+//}
+
+
